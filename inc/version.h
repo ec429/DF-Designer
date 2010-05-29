fbcb76c66011f0ea81635b630bc0813ad92c2ec8
Sound and Fury <ec429@cam.ac.uk>
2010-05-29 23:09:55 +0100

    Ignore inc/version.h as it is procedurally generated by Make

--- inc/version.h
+++
@@ -1,8 +0,0 @@
-//	designer - third party architectural design utility for 'Dwarf Fortress'
-//	Copyright (C) 2010 Edward Cree (see top of designer.c for license details)
-//   inc/version.h - contains version number
-#pragma once
-#define VERSION_MAJ	0	// Major version
-#define VERSION_MIN	14	// Minor version
-#define VERSION_REV	3	// Revision number; should be incremented before committing any source change which will affect binary behaviour
-#define VERSION_GIT	"5-gc8917fe"	// Rest of git describe
