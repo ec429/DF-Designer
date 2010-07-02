664a981e5f16fe382040721ee351bf70c35ab47b
Sound and Fury <ec429@cam.ac.uk>
2010-07-02 18:54:10 +0100

    Revert "Fixed segfault bug when groundlevel=0 (caused uslice=-1)"
    This reverts commit 9956277b80e02539de2adfaa36ceb655281a00b2.

--- src/designer.c
+++ src/designer.c
@@ -2422,7 +2422,7 @@ int load_map(char *filename, tile ****map, gui guibits, int *worldx, int *worldy
 					fscanf(fp, "%u,%u,%u,%u\n", levels, worldx, worldy, groundlevel);
 				else
 					fscanf(fp, "%u,%u,%u\n", levels, worldx, worldy);
-				*uslice=max(*groundlevel-1, 0);
+				*uslice=*groundlevel-1;
 				(*map) = (tile ***)realloc((*map), *levels*sizeof(tile **));
 				if((*map)==NULL)
 				{
